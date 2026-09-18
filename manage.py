7e09be426c0841e98e966c1054ce7879cdfa887b
Payal Tanpure <tanpurepayal91@example.com>
2026-09-18 16:23:05 +0530

    orm_new

--- manage.py
+++ manage.py
@@ -6,7 +6,11 @@ import sys
 
 def main():
     """Run administrative tasks."""
+<<<<<<< HEAD
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TODO.settings')
+=======
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'orm.settings')
+>>>>>>> 91a30d8 (orm)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
